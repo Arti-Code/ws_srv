87ecb6cbc85a14f44c269babb901233186a464a0
Arturo <41492101+Arti-Code@users.noreply.github.com>
2025-07-25 16:21:57 +0200

    v0.2.1

--- src/main.rs
+++ src/main.rs
@@ -35,58 +35,46 @@ type Tx = UnboundedSender<Message>;
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
 
 async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
-    println!("Incoming TCP connection from: {}", addr);
-
+    println!("new connection: {}", addr);
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
-        .expect("Error during the websocket handshake occurred");
-    println!("WebSocket connection established: {}", addr);
-
-    // Insert the write part of this peer to the peer map.
+        .expect("error during the websocket handshake occurred");
+    println!("websocket connection established: {}", addr);
     let (tx, rx) = unbounded();
     peer_map.lock().unwrap().insert(addr, tx);
-
     let (outgoing, incoming) = ws_stream.split();
-
     let broadcast_incoming = incoming.try_for_each(|msg| {
-        println!("Received a message from {}: {}", addr, msg.to_text().unwrap());
+        println!("{} => {}", addr, msg.to_text().unwrap());
         let peers = peer_map.lock().unwrap();
-
-        // We want to broadcast the message to everyone except ourselves.
         let broadcast_recipients =
             peers.iter().filter(|(peer_addr, _)| peer_addr != &&addr).map(|(_, ws_sink)| ws_sink);
-
         for recp in broadcast_recipients {
             recp.unbounded_send(msg.clone()).unwrap();
         }
-
         future::ok(())
     });
-
     let receive_from_others = rx.map(Ok).forward(outgoing);
-
     pin_mut!(broadcast_incoming, receive_from_others);
     future::select(broadcast_incoming, receive_from_others).await;
-
     println!("{} disconnected", &addr);
     peer_map.lock().unwrap().remove(&addr);
 }
 
 #[tokio::main]
 async fn main() -> Result<(), IoError> {
+    let ver = env!("CARGO_PKG_VERSION");
+    let author = env!("CARGO_PKG_AUTHORS");
+    let year = "2025 year";
+    println!("-=WEBSOCKET COMMUNICATION SERVER=-");
+    println!("ver: {} | {} | {}", ver, author, year);
+    println!("");
     let addr = env::args().nth(1).unwrap_or_else(|| "0.0.0.0:8080".to_string());
-
     let state = PeerMap::new(Mutex::new(HashMap::new()));
-
-    // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(&addr).await;
-    let listener = try_socket.expect("Failed to bind");
-    println!("Listening on: {}", addr);
-
-    // Let's spawn the handling of each connection in a separate task.
+    let listener = try_socket.expect("failed to bind");
+    println!("listening on: {}", addr);
     while let Ok((stream, addr)) = listener.accept().await {
         tokio::spawn(handle_connection(state.clone(), stream, addr));
     }
-
     Ok(())
 }
